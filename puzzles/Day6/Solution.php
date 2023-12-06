b8bf50b32493146d2714615a829c6e8e6b621e09
Floris <info@styxit.com>
2023-12-06 09:39:46 +0100

    Solve day 6

---
+++ puzzles/Day6/Solution.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Puzzles\Day6;
+
+use Illuminate\Support\Collection;
+use Styxit\Input;
+use Styxit\PuzzleSolutionInterface;
+
+class Solution implements PuzzleSolutionInterface
+{
+    /**
+     * Find the solution for part 1.
+     */
+    public function solution1(Input $input)
+    {
+        $raceRecords = $this->getRaceRecordsFromInput($input);
+
+        $waysToBeat = $raceRecords->map(function ($race) {
+            return $this->getRaceWins($race['time'], $race['distance']);
+        });
+
+        return array_product($waysToBeat->toArray());
+    }
+
+    /**
+     * Find the solution for part 2.
+     */
+    public function solution2(Input $input)
+    {
+        $raceTime = $this->getCombinedNumberFromString($input->lines()[0]);
+        $raceDistance = $this->getCombinedNumberFromString($input->lines()[1]);
+
+        return $this->getRaceWins($raceTime, $raceDistance);
+    }
+
+    private function getRaceWins(int $time, int $recordDistance)
+    {
+        $winCount = 0;
+
+        for ($ms = 1; $ms < $time; ++$ms) {
+            $speed = $ms;
+            $duration = $time - $ms;
+            $distance = $speed * $duration;
+
+            if ($distance > $recordDistance) {
+                ++$winCount;
+            }
+        }
+
+        return $winCount;
+    }
+
+    private function getCombinedNumberFromString(string $inputString): int
+    {
+        return (int) preg_replace('/\D/', '', $inputString);
+    }
+
+    private function getRaceRecordsFromInput(Input $inputString): Collection
+    {
+        $timeString = $inputString->lines()[0];
+        $distanceString = $inputString->lines()[1];
+
+        // Turn number string into a collection of individual numbers.
+        $times = new Collection(array_map('intval', preg_split('/\s+/', $timeString)));
+        unset($times[0]);
+
+        $distances = new Collection(array_map('intval', preg_split('/\s+/', $distanceString)));
+        unset($distances[0]);
+
+        $records = new Collection();
+        foreach ($times as $key => $time) {
+            $records->add([
+                'time' => $time,
+                'distance' => $distances[$key],
+            ]);
+        }
+
+        return $records;
+    }
+}
---
+++ tests/Solutions/Day6Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Solutions;
+
+/**
+ * @internal
+ */
+class Day6Test extends AbstractDayTester
+{
+    protected $solutionPart1 = 2374848;
+    protected $solutionPart2 = 39132886;
+
+    protected $exampleSolution1 = 288;
+    protected $exampleSolution2 = 71503;
+}
